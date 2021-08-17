44c80d6d7834d590ffdf347f5958645de148e568
Sowmiya Devaraj <sowmidevaraj@gmail.com>
2021-08-18 02:25:04 +0530

    Finalized and documented source code

--- Source_Code/Drowsiness_Detection.py
+++ Source_Code/Drowsiness_Detection.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import Flatten, GlobalAveragePooling2D
 from sklearn.metrics import recall_score, precision_score, confusion_matrix
 from tensorflow.keras.models import Model
 from sklearn.metrics import accuracy_score
-from sklearn import svm
+from sklearn.svm import SVC
 import numpy as np
 import time
 from sklearn.decomposition import PCA
@@ -19,43 +19,130 @@ import matplotlib.pyplot as plt
 
 
 def image_augmentation(x_train, y_train):
+    """A function for augmenting the newly populated images.
+           Parameters
+           ----------
+           x_train : The x_train images of ndarray.
+           y_train : The y_train class labels of ndarray.
+
+           Steps
+           ----------
+           STEP 1 : Populates 100 images per class using the created populate_images() function.
+           STEP 2 : Uses an in-built ImageDataGenerator() method to do a flip and shift the width and height of images.
+           STEP 3 : Obtains the newly augmented images.
+           STEP 4 : Combine the newly augmented images and its labels to the untouched x_train images.
+
+           Returns
+           ----------
+           nd-array:
+                x_train_augmented with combined augmented and original x_train images.
+                y_train_augmented with the x_train_augmented labels.
+           """
+
     images, labels = populate_images(x_train, y_train)
     data_generator = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.2, horizontal_flip=True, vertical_flip=True)
     train_generator = data_generator.flow(images, labels, batch_size=500)
     aug_images = train_generator[0][0]
     aug_labels = train_generator[0][1]
-    augmented_x_train = np.vstack((x_train, aug_images))
-    augmented_y_train = np.hstack((y_train, aug_labels))
-    return augmented_x_train, augmented_y_train
+    x_train_augmented = np.vstack((x_train, aug_images))
+    y_train_augmented = np.hstack((y_train, aug_labels))
+
+    return x_train_augmented, y_train_augmented
 
 
-def VGG_model(augmented_x_train, x_test):
+def VGG16_model(x_train_augmented, x_test):
+    """A function extract deep features from the VGG16 model.
+           Parameters
+           ----------
+           x_train_augmented : The x_train images of ndarray to extract deep features.
+           x_test : The x_train images of ndarray to extract deep features.
+
+           Steps
+           ----------
+           STEP 1 : Downloads the VGG16 CNN model with input shape=(224, 224, 3), imagenet weights
+                    and without the fully connected layer
+           STEP 2 : Use the GlobalAveragePooling2D() function to summarize the important features maps.
+           STEP 3 : Construct the model of VGG16.
+           STEP 4 : Input the x_train_augmented and x_test to extract deep features
+
+           Returns
+           ----------
+           nd-array:
+                vgg_train_deep_features : Extracted train deep features from the pre-trained imagenet
+                                          CNN model.
+                vgg_test_deep_features : Extracted test deep features from the pre-trained imagenet
+                                         CNN model.
+           """
+
     base = VGG16(input_shape=(224, 224, 3), weights='imagenet', include_top=False)
     out = base.output
     out = GlobalAveragePooling2D()(out)
     out = Flatten()(out)
-    model = Model(inputs=base.input, outputs=out)
+    vgg = Model(inputs=base.input, outputs=out)
     # extracting deep features on the x_train and x_test
-    vgg_train_deep_features = model.predict(augmented_x_train)
-    vgg_test_deep_features = model.predict(x_test)
+    vgg_train_deep_features = vgg.predict(x_train_augmented)
+    vgg_test_deep_features = vgg.predict(x_test)
 
     return vgg_train_deep_features, vgg_test_deep_features
 
 
-def ResNet_model(augmented_x_train, x_test):
+def ResNet101_model(x_train_augmented, x_test):
+    """A function extract deep features from the RESNET101 model.
+           Parameters
+           ----------
+           x_train_augmented : The x_train images of ndarray to extract deep features.
+           x_test : The x_train images of ndarray to extract deep features.
+
+           Steps
+           ----------
+           STEP 1 : Downloads the ResNet101 model with input shape=(224, 224, 3), imagenet weights
+                    and without the fully connected layer.
+           STEP 2 : Use the GlobalAveragePooling2D() function to summarize the important features maps.
+           STEP 3 : Construct the model of ResNet101.
+           STEP 4 : Input the x_train_augmented and x_test to extract deep features.
+
+           Returns
+           ----------
+           nd-array:
+                resnet_train_deep_features : Extracted train deep features from the pre-trained imagenet
+                                             CNN model.
+                resnet_test_deep_features : Extracted test deep features from the pre-trained imagenet
+                                            CNN model.
+           """
+
     base = ResNet101(input_shape=(224, 224, 3), weights='imagenet', include_top=False)
     out = base.output
     out = GlobalAveragePooling2D()(out)
     out = Flatten()(out)
-    model = Model(inputs=base.input, outputs=out)
+    resnet_model = Model(inputs=base.input, outputs=out)
     # extracting deep features on the x_train and x_test
-    resnet_train_deep_features = model.predict(augmented_x_train)
-    resnet_test_deep_features = model.predict(x_test)
+    resnet_train_deep_features = resnet_model.predict(x_train_augmented)
+    resnet_test_deep_features = resnet_model.predict(x_test)
 
     return resnet_train_deep_features, resnet_test_deep_features
 
 
 def feature_combination(vgg_train_deep_features, vgg_test_deep_features, resnet_train_deep_features, resnet_test_deep_features):
+    """A function to combine the extracted deep features from the vgg16 and resnet101.
+           Parameters
+           ----------
+           vgg_train_deep_features : Extracted train deep features from the pre-trained imagenet CNN model.
+           vgg_test_deep_features : Extracted test deep features from the pre-trained imagenet CNN model.
+           resnet_train_deep_features : Extracted train deep features from the pre-trained imagenet CNN model.
+           resnet_test_deep_features : Extracted test deep features from the pre-trained imagenet CNN model.
+
+           Steps
+           ----------
+           STEP 1 : Combines the vgg and resnet train features together horizontally(column-wise).
+           STEP 2 : Combines the vgg and resnet test features together horizontally(column-wise).
+
+           Returns
+           ----------
+           nd-array:
+                x_train_deep_features : Combined train deep feature from the vgg16 and resnet101.
+                x_test_deep_features : Combined test deep feature from the vgg16 and resnet101.
+           """
+
     # combining the train deep features
     x_train_deep_features = np.hstack((vgg_train_deep_features, resnet_train_deep_features))
     # combining the test deep features
@@ -64,59 +151,102 @@ def feature_combination(vgg_train_deep_features, vgg_test_deep_features, resnet_
     return x_train_deep_features, x_test_deep_features
 
 
-def pricipal_component_analysis(x_train_deep_features, x_test_deep_features, train_y):
+def pricipal_component_analysis(x_train_deep_features, x_test_deep_features, y_train_augmented):
+    """A function to use Principal component analysis (Dimentionality reduction) method.
+          Parameters
+          ----------
+          x_train_deep_features : Combined train deep feature from the vgg16 and resnet101.
+          x_test_deep_features : Combined test deep feature from the vgg16 and resnet101.
+
+          Steps
+          ----------
+          STEP 1 : Calling an object using an in-built PCA method.
+          STEP 2 : fit_transfom() the x_train_deep_features and transform() x_test_deep_features to reduce
+                   the dimension of the features.
+
+          Returns
+          ----------
+          nd-array:
+               x_train_deep_features : Dimension reduced train deep features.
+               x_test_deep_features : Dimension reduced test deep features.
+          """
+
     pca = PCA()
-    x_train_deep_features = pca.fit_transform(x_train_deep_features, train_y)
+    x_train_deep_features = pca.fit_transform(x_train_deep_features, y_train_augmented)
     x_test_deep_features = pca.transform(x_test_deep_features)
 
     return x_train_deep_features, x_test_deep_features
 
 
-def Svm_Classifier(x_train_deep_features, x_test_deep_features, train_y, y_test):
+def Svm_Classifier(x_train_deep_features, x_test_deep_features, y_train_augmented, y_test):
+    """A function to classify and evaluate using SVM Classifier .
+          Parameters
+          ----------
+          x_train_deep_features : Combined and Dimension reduced train deep features.
+          x_test_deep_features : Combined and Dimension reduced test deep features.
+
+          Steps
+          ----------
+          STEP 1 : Calling an in-built SVM classifier form sk-learn with "rbf" kernel and C=0.9.
+          STEP 2 : Train the classifier using fit() method with x_train_deep_features and y_train_augmented values.
+          STEP 3 : Record the training time to print
+          STEP 4 : Predict the classifier using predict() method with x_train_deep_features and y_train_augmented values
+                   which will output the predicted y test values.
+          STEP 5 : Evaluate and print the SVM model using accuracy, precision, recall and confusion matrix values.
+
+          Returns
+          ----------
+          nd-array:
+               x_train_deep_features : Dimension reduced train deep features.
+               x_test_deep_features : Dimension reduced test deep features.
+          """
 
     # initializing the svm classifier
-    classifier = svm.SVC(kernel='rbf', C=0.9)
+    classifier = SVC(kernel='rbf', C=0.9)
 
-    # training the classifier
+    # start train time
     start_time = time.process_time()
 
-    classifier.fit(x_train_deep_features, train_y)
+    # training the classifier
+    print("-------------------------------")
+    print(" Training using SVM ")
+    classifier.fit(x_train_deep_features, y_train_augmented)
     y_train_predicted = classifier.predict(x_train_deep_features)
 
+    # end train time
     end_time = time.process_time()
 
+    # calculate train time
     training_time = end_time - start_time
 
     # testing the classifier
-    y_predicted = classifier.predict(x_test_deep_features)
+    y_test_predicted = classifier.predict(x_test_deep_features)
+    print("-------------------------------")
+    print(" Testing using SVM")
+    print("-------------------------------")
 
-
-    """" Evaluating SVM model """""
-    # Evaluate the SVM model using accuracy, precision, recall and confusion matrix
-    training_accuracy = accuracy_score(train_y, y_train_predicted)
-    testing_accuracy = accuracy_score(y_test, y_predicted)
-    precision = precision_score(y_test, y_predicted, average='macro')
-    recall = recall_score(y_test, y_predicted, average='macro')
-    con_mat = confusion_matrix(y_test, y_predicted)
+    # Evaluate and print the SVM model using accuracy, precision, recall and confusion matrix
+    training_accuracy = accuracy_score(y_train_augmented, y_train_predicted)
+    testing_accuracy = accuracy_score(y_test, y_test_predicted)
+    precision = precision_score(y_test, y_test_predicted, average='macro')
+    recall = recall_score(y_test, y_test_predicted, average='macro')
+    con_mat = confusion_matrix(y_test, y_test_predicted)
 
     print("Training time :", training_time)
     print("Training Accuracy : {}".format(training_accuracy))
     print("Testing Accuracy : {}".format(testing_accuracy))
     print("Precision : {}".format(precision))
     print("Recall : {}".format(recall))
-    # print("F1-Score : {}".format(f1score))
     print("confusion matrix", con_mat)
-
-
-def data_visualization():
-    pass
-
+    print("-------------------------------")
 
 
 def main():
+    # specifies the face path and face_cascade_path
     face_path = "../Dataset/Face"
     face_cascade_path = "../Cascade_Classifier/haarcascade_frontalface_default.xml"
 
+    # specifies the eye path and eye_cascade_path
     eye_path = "../Dataset/Eyes"
     eye_cascade_path = "../Cascade_Classifier/haarcascade_eye.xml"
 
@@ -127,31 +257,31 @@ def main():
     # Data Preprocessing
     X = np.concatenate((face_image, eye_image), axis=0)
     y = np.concatenate((face_label, eye_label), axis=0)
+
+    # Processing label encoding for class labels
     Y = label_encoder(y)
 
-    # Making train test split
+    # Making 70:30 train-test split
     x_train, x_test, y_train, y_test = split(X, Y)
 
-    # image data argumentation on the train dataset
-    augmented_x_train, augmented_y_train = image_augmentation(x_train, y_train)
+    # Performing Image data argumentation on the train dataset
+    x_train_augmented, y_train_augmented = image_augmentation(x_train, y_train)
 
-    # extracting deep features using VGG-16 places365 weights
-    vgg_train_deep_features, vgg_test_deep_features = VGG_model(augmented_x_train, x_test)
+    # Extracting deep features using pre-trained VGG-16 with imagenet weights
+    vgg_train_deep_features, vgg_test_deep_features = VGG16_model(x_train_augmented, x_test)
 
-    # extracting deep features using ResNet101  weights
-    resnet_train_deep_features, resnet_test_deep_features = ResNet_model(augmented_x_train, x_test)
+    # Extracting deep features using pre-trained ResNet101 with imagenet  weights
+    resnet_train_deep_features, resnet_test_deep_features = ResNet101_model(x_train_augmented, x_test)
 
-    # feature combination
+    # Combination VGG16 and Resnet101 deep features
     x_train_deep_features, x_test_deep_features = feature_combination(vgg_train_deep_features, vgg_test_deep_features,
                                                                       resnet_train_deep_features,resnet_test_deep_features)
 
-    x_train_deep_features, x_test_deep_features = pricipal_component_analysis(x_train_deep_features, x_test_deep_features, augmented_y_train)
+    # Using PCA for dimentionality reduction
+    x_train_deep_features, x_test_deep_features = pricipal_component_analysis(x_train_deep_features, x_test_deep_features, y_train_augmented)
 
-
-    # predicting using SVM classifier
-    Svm_Classifier(x_train_deep_features, x_test_deep_features, augmented_y_train, y_test)
-
-    temp=0
+    # predicting and evaluating metrics using SVM classifier
+    Svm_Classifier(x_train_deep_features, x_test_deep_features, y_train_augmented, y_test)
 
 
 if __name__ == "__main__":
--- Source_Code/face_eye_detector.py
+++ Source_Code/face_eye_detector.py
@@ -6,8 +6,30 @@ import cv2
 import os
 
 
-# face detector function
 def face_detector(root, face_cascade_path):
+    """A function to read the images from root directory and detect faces from images.
+           Parameters
+           ----------
+           root : root directory path, mandatory
+               The root specifies the path of the file containing images.
+
+           face_cascade_path : face cascade classifier directory, mandatory
+               The face_cascade_path specifies path of the Harr-Cascade classifier path to detect the faces.
+
+           Steps
+           ----------
+           STEP 1 : Reads each image in "RBG" format from the specified root path directory.
+           STEP 2 : Implements the in-built face detector from the the Haar-Classifier.
+           STEP 3 : Fetches 4 coordinate points and constructs the rectangle box around detected faces
+           STEP 4 : Resize the image to 224 * 224.
+           STEP 5 : Appends both the detected faces and its respective folder name as class_labels.
+
+           Returns
+           ----------
+           list
+                two lists: one list of extracted face images (array) and one list of its respective class labels.
+           """
+
     image_size = 224
     face_image, face_label = [], []
     for parentPath, subdirs, files in os.walk(root):
@@ -28,11 +50,33 @@ def face_detector(root, face_cascade_path):
                     resized_array = cv2.resize(roi_color, (image_size, image_size))
                     face_image.append(resized_array)
                     face_label.append(subdir)
+
     return face_image, face_label
 
 
-# eye detector function
 def eye_detector(root, eye_cascade_path):
+    """A Function to read the images from root directory and detect eyes from images.
+           Parameters
+           ----------
+           root : root directory path, mandatory
+               The root specifies the path of the file containing images.
+
+           eye_cascade_path : eye cascade classifier directory, mandatory
+               The eye_cascade_path specifies path of the Harr-Cascade classifier path to detect the eyes.
+
+           Steps
+           ----------
+           STEP 1 : Reads each image in "RBG" format from the specified root path directory.
+           STEP 2 : Fetches eye image and constructs the rectangle box around detected faces
+           STEP 3 : Resize the image to 224 * 224.
+           STEP 4 : Appends both the detected eyes and its respective folder name as class_labels.
+
+           Returns
+           ----------
+           list
+                two lists: one list of extracted eye images (array) and one list of its respective class labels.
+           """
+
     eye_image, eye_label = [], []
     for parentPath, subdirs, files in os.walk(root):
         for subdir in subdirs:
@@ -43,24 +87,75 @@ def eye_detector(root, eye_cascade_path):
                 img = image.load_img(imgPath, target_size=(224, 224))
                 eye_image.append(image.img_to_array(img))
                 eye_label.append(subdir)
+
     return eye_image, eye_label
 
 
-# Label encoder function for encoding the labels
 def label_encoder(y):
+    """A Function convert the string labels to numeric labels.
+          Parameters
+          ----------
+          y : y is the class label that needs to be encoded to numeric values.
+
+          Steps
+          ----------
+          STEP 1 : Creates an object with in-built LabelEncoder() function.
+          STEP 2 : Transforms the sting labels to numeric labels using in-buit fit_transform() function.
+
+          Returns
+          ----------
+          y
+               A ndarray with the numeric labels.
+          """
+
     label_encode = LabelEncoder()
     y = label_encode.fit_transform(y)
+
     return y
 
 
-# test train function
 def split(x, y):
+    """A Function to make a 70 : 30 train test split .
+          Parameters
+          ----------
+          x : x is the total image dataset.
+          y : y is the total class label.
+
+          Steps
+          ----------
+          STEP 1 : Uses an in-built train_test_split() function to split the whole images into 70 : 30.
+
+          Returns
+          ----------
+          x_train, x_test, y_train, y_test
+               x_train and y_train with 70 % of the data.
+               x_test and y_test with 30 % of the data.
+          """
+
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42, shuffle=True)
+
     return x_train, x_test, y_train, y_test
 
 
-# function to create duplicate images form the training dataset for the image_augmentation
 def populate_images(x, y):
+    """A function to create duplicate images form the training dataset for the image_augmentation.
+          Parameters
+          ----------
+          x : x is the image dataset.
+          y : y is the class label.
+
+          Steps
+          ----------
+          STEP 1 : Finds the unique class labels.
+          STEP 2 : Fetches the 100 images per class
+          STEP 3 : Appends the populate images and its labels to a list.
+
+          Returns
+          ----------
+          images : 100 images per class is returned.
+          labels : The lables of the images are returned.
+          """
+
     images, labels = [], []
     unique_labels = np.unique(y)
     for i in unique_labels:
@@ -75,6 +170,7 @@ def populate_images(x, y):
             labels.append(y_values)
     images = np.array(images)
     labels = np.array(labels)
+
     return images, labels
 
 
